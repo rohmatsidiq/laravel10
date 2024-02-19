5169722877269493ef78339e359fd16d5f5e181c
Rohmat Sidiq Mustaqim <rohmatsidiq9@gmail.com>
2024-02-19 16:50:53 +0700

    install admin lte

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+
     public function index()
     {
         $data = User::get();
